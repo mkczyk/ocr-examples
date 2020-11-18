992ef338016d48f07703409ac5058194779bf8d7
Marcin Kowalczyk <mkczyk@gmail.com>
2020-11-18 21:08:12 +0100

    Project skeleton
    - Generated in Spring Initializr
    - Java 11, Spring, Maven

---
+++ src/main/java/pl/marcinkowalczyk/ocr/examples/OcrExamplesApplication.java
@@ -0,0 +1,13 @@
+package pl.marcinkowalczyk.ocr.examples;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OcrExamplesApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(OcrExamplesApplication.class, args);
+    }
+
+}
---
+++ src/test/java/pl/marcinkowalczyk/ocr/examples/OcrExamplesApplicationTests.java
@@ -0,0 +1,13 @@
+package pl.marcinkowalczyk.ocr.examples;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OcrExamplesApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
